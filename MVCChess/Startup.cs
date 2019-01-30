c1e0ee482d75e54eeab94bc76a0ae76ce321cd30
KF <kevin.fagot@edu.itescia.fr>
2019-01-30 16:04:14 +0100

    Add sessions

--- MVCChess/Startup.cs
+++ MVCChess/Startup.cs
@@ -26,6 +26,14 @@ namespace MVCChess {
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             }
@@ -43,7 +51,7 @@ namespace MVCChess {
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
+            app.UseSession();
             app.UseMvc(routes => {
                 routes.MapRoute(
                     name: "default",
